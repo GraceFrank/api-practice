e437b06ce729b59a8ab7c4164e29dcf087cae4a2
Grace Frank <frankgracey@gmail.com>
2018-12-12 12:40:51 +0100

    added first function

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 $(document).ready(function(){
 
+        
     $('#submitButton').on('click', function(){
-      
+      var input =$('#wordInput').val();
 
         $.ajax({
             type:'GET',
             url:'https://jsonplaceholder.typicode.com/todos',
             success:function(data){
                 alert("success");
+                
+                for(let i =0; i<data.length; i++){
+                    if(data[i].title.includes(input)){
+                        console.log(data[i])
+                    }
+
+                    }//end of for loop
                 },
             error:function(){
                 console.log("error getting details");
