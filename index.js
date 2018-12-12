f018674938b5b802415a4547ac8f24ccc81171e0
Grace Frank <frankgracey@gmail.com>
2018-12-12 13:00:38 +0100

    added another function for the find data with id and modified my first function

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 $(document).ready(function(){
 
-function findText(){
+    function findNumber(input){
+        $.ajax({
+            type:'GET',
+            url:'https://jsonplaceholder.typicode.com/todos',
+            success:function(data){
+                alert("success");
+                
+                for(let i =0; i<data.length; i++){
+                    if(data[i].id == input){console.log(data[i])}
+    
+                    }//end of for loop
+                },
+            error:function(){
+                console.log("error getting details");
+            }
+            });
+        }//end of findNumber
+
+function findText(input){
     $.ajax({
         type:'GET',
         url:'https://jsonplaceholder.typicode.com/todos',
@@ -22,10 +40,14 @@ function findText(){
         
     $('#submitButton').on('click', function(){
       var input =$('#wordInput').val();
-
+        findText(input);
         
-
-
         });
+
+     $('#submitButton2').on('click', function(){
+        var input =$('#NumberInput').val();
+              findNumber(input);
+              
+            });
 
 });
