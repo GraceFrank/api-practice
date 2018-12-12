69988422206f2b0c5bdf1053379a19c3d81c0ee5
Grace Frank <frankgracey@gmail.com>
2018-12-12 12:45:30 +0100

    added another form for numbers

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
 $(document).ready(function(){
 
+function findText(){
+    $.ajax({
+        type:'GET',
+        url:'https://jsonplaceholder.typicode.com/todos',
+        success:function(data){
+            alert("success");
+            
+            for(let i =0; i<data.length; i++){
+                if(data[i].title.includes(input)){
+                    console.log(data[i])
+                }
+
+                }//end of for loop
+            },
+        error:function(){
+            console.log("error getting details");
+        }
+        });
+}
         
     $('#submitButton').on('click', function(){
       var input =$('#wordInput').val();
 
-        $.ajax({
-            type:'GET',
-            url:'https://jsonplaceholder.typicode.com/todos',
-            success:function(data){
-                alert("success");
-                
-                for(let i =0; i<data.length; i++){
-                    if(data[i].title.includes(input)){
-                        console.log(data[i])
-                    }
-
-                    }//end of for loop
-                },
-            error:function(){
-                console.log("error getting details");
-            }
-            });
+        
 
 
         });
